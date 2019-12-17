76b1d6d4073e17c15672b8480505ba00d249124e
Dave Mickelsen <iamtheweasel@gmail.com>
2019-12-16 22:35:12 -0700

    Updated to run on heroku, added display fix to breadcrumb arrows.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MustMatch } from 'src/app/helpers/must-match.validator';
 
 @Component({
   selector: 'app-home',
